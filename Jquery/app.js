0b7738b31cc95c686e82418d0ac7c25daf855624
D3RL3 <tvboxm9sx@gmail.com>
2019-12-01 13:18:52 -0500

    Se termina la carpeta de javascript y se crea carpeta de jquery

---
+++ Jquery/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+//    $('h1').html('Esta es mi etiqueta h1');
+//    $('.display-4').html('desde la clase');
+//    $('#idh1').html('desde mi id');
+
+//    document.querySelector('h1').innerHTML = 'Etiqueta h1 desde javascript';
+//    document.querySelector('.display-4').innerHTML = 'Etiqueta display desde javascript';
+//    document.querySelector('#idh1').innerHTML = 'Etiqueta h1 desde javascript';
+// $('.container h1').html('cambio de texto');
+// $('.container h1:first').html('cambio de texto');
+// $('#idh1').addClass('text-danger');
+// $('#idh1').removeClass('display-4');
+
+// $('#contenido').append('<h1>Otro h1 con appent</h1>');
+// $('#contenido').prepend('<h1>Otro h1 con prepend</h1>');
+// $('#color-azul').css('color', 'blue');
+// $('#color-azul').css({color: 'lightgreen', background: 'black', padding: '20px'});
+// $('#color-azul').remove();
+// $('#color-azul').hide();
+// $('img').attr('src', 'https://storage.googleapis.com/wp-stateless/2019/11/Vanity-TV-Mirror-Master-Bath.jpg');
+// $('img:last').attr({width: '500px', hight: '500px'});
+// var parrafo = $('#resultado p');
+
+// $('.btn-primary').click(function () { 
+//     parrafo.addClass('display-4');
+    
+// });
+
+// $('.btn-danger').click(function () { 
+//     parrafo.removeClass('display-4');
+    
+// });
+// $('.btn-warning').click(function () { 
+//     parrafo.toggleClass('display-4')
+    
+// });
+
+
+});
